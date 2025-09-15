148f1b093a9cd02d058373eabba6cfa5d573922e
Paula Babbicola <5652360+pbabbicola@users.noreply.github.com>
2025-09-15 23:14:44 +0200

    Create go mod and main function.

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// The website monitor should perform the checks periodically and collect the request timestamp,
+// the response time, the HTTP status code, as well as optionally checking the returned page
+// contents for a regex pattern that is expected to be found on the page. Each URL should be
+// checked periodically, with the ability to configure the interval (between 5 and 300 seconds) and
+// the regexp on a per-URL basis. The monitored URLs can be anything found online. In case the
+// check fails the details of the failure should be logged into the database.
+
+func main() {
+	rootCmd := &cobra.Command{
+		Use:     "gomonitor",
+		Short:   "Go Monitor checks a list of websites periodically.",
+		Long:    "Go Monitor checks a list of websites periodically.",
+		Example: "gomonitor --interval 5 --regexp a.* url-list.json",
+	}
+
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
